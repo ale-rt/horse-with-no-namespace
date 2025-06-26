c3860e1d70cdcf08824786203fa919aa520138ba
ale-rt <alessandro.pisa@gmail.com>
2025-06-26 16:58:46 +0200

    Limit namespaces using an env variable

--- src/horse_with_no_namespace/__init__.py
+++ src/horse_with_no_namespace/__init__.py
@@ -13,9 +13,25 @@
 # and that hasn't changed recently.)
 
 import pkgutil
+import os
 import sys
 
 logged = False
+
+limit_namespaces = os.getenv("HORSE_WITH_NO_NAMESPACES")
+if limit_namespaces is not None:
+    limit_namespaces = set(limit_namespaces.split(","))
+
+_message = (
+    "🐎 This Python uses horse-with-no-namespace "
+    "to make pkg_resources namespace packages compatible "
+    "with PEP 420 namespace packages."
+)
+
+if limit_namespaces:
+    _message += " It is limited to the following namespaces: " + ", ".join(
+        limit_namespaces
+    )
 
 
 def apply():
@@ -24,12 +40,7 @@ def apply():
     # But we only want to show the notice once.
     global logged
     if not logged:
-        print(
-            "🐎 This Python uses horse-with-no-namespace "
-            "to make pkg_resources namespace packages compatible "
-            "with PEP 420 namespace packages.",
-            file=sys.stderr,
-        )
+        print(_message, file=sys.stderr)
         logged = True
 
     # Only patch pkg_resources if it is installed...
@@ -40,7 +51,28 @@ def apply():
     else:
         # Patch pkg_resources.declare_namespace
         # to update __path__ using pkgutil.extend_path instead
+        try:
+            pkg_resources._original_declare_namespace
+        except AttributeError:
+            pkg_resources._original_declare_namespace = pkg_resources.declare_namespace
+
         def declare_namespace(packageName):
+            if limit_namespaces:
+                if (
+                    not any(
+                        packageName.startswith(prefix + ".")
+                        for prefix in limit_namespaces
+                    )
+                    or packageName not in limit_namespaces
+                ):
+                    return pkg_resources._original_declare_namespace(packageName)
+
+            if limit_namespaces:
+                print(
+                    f"🐎 Declaring namespace package {packageName} "
+                    "using pkgutil.extend_path.",
+                    file=sys.stderr,
+                )
             parent_locals = sys._getframe(1).f_locals
             if not "__path__" in parent_locals:
                 parent_locals["__path__"] = []
@@ -53,6 +85,19 @@ def apply():
     # Remove existing namespace package modules that were already created
     # by other .pth files, possibly with an incomplete __path__
     for name, module in list(sys.modules.items()):
+        if (
+            limit_namespaces
+            and not any(name.startswith(prefix + ".") for prefix in limit_namespaces)
+            and name not in limit_namespaces
+        ):
+            continue
+
+        if limit_namespaces:
+            print(
+                f"🐎 Removing existing namespace package {name} "
+                "to ensure it is compatible with PEP 420.",
+                file=sys.stderr,
+            )
         loader = getattr(module, "__loader__", None)
         if loader and loader.__class__.__name__ == "NamespaceLoader":
             del sys.modules[name]
